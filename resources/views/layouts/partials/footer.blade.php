4fa7d177d37cff5433412a20e874498d9df9f600
Athul <rkathul@gmail.com>
2026-08-10 14:59:16 +0530

    Remove admin panel views and associated scripts, refactor routes to use HomeController for landing page

--- resources/views/layouts/partials/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<footer class="app-footer">
-  <div class="float-end d-none d-sm-inline">Admin panel layout</div>
-  <strong>
-    Copyright &copy; {{ date('Y') }}&nbsp;
-    <a href="#" class="text-decoration-none">{{ config('app.name', 'L13') }}</a>.
-  </strong>
-  All rights reserved.
-</footer>
--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,7 @@
 <?php
 
-use App\Http\Controllers\Admin\DashboardController;
-use App\Http\Controllers\Admin\LoginController;
-use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return redirect()->route('admin.login');
-});
+Route::get('/', [HomeController::class, 'index']);
 
-Route::prefix('admin')->name('admin.')->group(function () {
-    Route::get('/login', [LoginController::class, 'show'])->name('login');
-    Route::post('/login', [LoginController::class, 'login'])->name('login.post');
-    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-    Route::middleware('auth')->group(function () {
-        Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-        Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
-        Route::get('/users', [UserController::class, 'index'])->name('users.index');
-        Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-        Route::post('/users', [UserController::class, 'store'])->name('users.store');
-        Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
-        Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
-        Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
-    });
-});
